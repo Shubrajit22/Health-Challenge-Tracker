baacea4ae488bd75e567ec656deb85918ff96d21
Shubrajit <shubrajitdeb180603@gmail.com>
2025-01-29 14:04:33 +0530

    html for the form created

---
+++ src/app/inputcomponent/inputcomponent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inputcomponent',
+  imports: [],
+  templateUrl: './inputcomponent.component.html',
+  styleUrl: './inputcomponent.component.css'
+})
+export class InputcomponentComponent {
+
+}
